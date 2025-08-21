8d464e2f564816042ba5bb5d99c7e74f40fe419d
Matej Matuska <mmatuska@redhat.com>
2025-08-21 12:16:29 +0200

    checkfips: Remove RHEL 7 path

--- repos/system_upgrade/common/actors/checkfips/actor.py
+++ repos/system_upgrade/common/actors/checkfips/actor.py
@@ -1,4 +1,3 @@
-from leapp import reporting
 from leapp.actors import Actor
 from leapp.exceptions import StopActorExecutionError
 from leapp.libraries.common.config import version
@@ -24,24 +23,7 @@ class CheckFips(Actor):
                                           details={'Problem': 'Did not receive a message with information about FIPS '
                                                               'usage'})
 
-        if version.get_target_major_version() == '8':
-            if fips_info.is_enabled:
-                title = 'Automated upgrades from RHEL 7 to RHEL 8 in FIPS mode are not supported'
-                summary = ('Leapp has detected that FIPS is enabled on this system. '
-                           'Automated in-place upgrade of RHEL 7 systems in FIPS mode is currently unsupported '
-                           'and manual intervention is required.')
-
-                fips_7to8_steps_docs_url = 'https://red.ht/planning-upgrade-to-rhel8'
-
-                reporting.create_report([
-                    reporting.Title(title),
-                    reporting.Summary(summary),
-                    reporting.Severity(reporting.Severity.HIGH),
-                    reporting.Groups([reporting.Groups.SECURITY, reporting.Groups.INHIBITOR]),
-                    reporting.ExternalLink(url=fips_7to8_steps_docs_url,
-                                           title='Planning an upgrade from RHEL 7 to RHEL 8')
-                ])
-        elif version.get_target_major_version() == '9':
+        if version.get_target_major_version() == '9':
             # FIXME(mhecko): We include these files manually as they are not included automatically when the fips
             # module is used due to a bug in dracut. This code should be removed, once the dracut bug is resolved.
             # See https://bugzilla.redhat.com/show_bug.cgi?id=2176560
