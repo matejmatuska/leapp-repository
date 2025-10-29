416b92574117773e4b94908fbc3c9c90afada8a0
Matej Matuska <mmatuska@redhat.com>
2025-10-29 19:31:00 +0100

    Drop dependency on mock
    The mock library is now a part of the Python standard library since
    Python 3.3 and is available as the unittest.mock. The 'mock' library is
    only a backport which is not needed anymore.
    
    Also remove some unnecessary imports.

--- repos/system_upgrade/common/actors/persistentnetnamesconfig/tests/test_persistentnetnamesconfig.py
+++ repos/system_upgrade/common/actors/persistentnetnamesconfig/tests/test_persistentnetnamesconfig.py
@@ -12,7 +12,6 @@ from leapp.models import (
     PCIAddress,
     PersistentNetNamesFacts,
     PersistentNetNamesFactsInitramfs,
-    RenamedInterface,
     RenamedInterfaces,
     TargetInitramfsTasks
 )
